35c9f683d63fdd2ad01c95042cf7020fd565deed
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-04-24 14:04:39 +0700

    Handle the exception

--- resumo-web/app/controllers/application_controller.rb
+++ resumo-web/app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionView::Rendering
   include ActionController::MimeResponds
+
+  rescue_from StandardError, with: :render_500
+
+  def render_500(exception)
+    render 'errors/500', locals: { exception: exception }, status: :internal_server_error
+  end
 end
